94a968bca1afddfad292a993ec2096f4b7498691
sefauncuetiya <sefa.uncu@etiya.com>
2019-12-30 09:30:51 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'heroku-ui';
+  title: any;
+
+  constructor(private http: HttpClient) {
+    this.http.get<any>('https://heroku-smart.herokuapp.com/home').subscribe((resp) => {
+      console.log(resp);
+      this.title = resp.toString();
+    });
+  }
+
 }
+
