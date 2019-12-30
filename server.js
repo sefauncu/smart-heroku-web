ed350334db1e5e0b8a58ae28a8d5015f865afed5
sefauncuetiya <sefa.uncu@etiya.com>
2019-12-30 10:35:07 +0300

    fix

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/dist/heroku-ui'));
+
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+
+  res.sendFile(path.join(__dirname + '/dist/heroku-ui/index.html'));
 });
-console.log('Console listening!');
+
 // Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
